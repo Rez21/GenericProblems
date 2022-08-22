f2781c0acc7d28594e6450d291de363b472c599a
Prasad <banprasad21@gmail.com>
2022-08-23 02:35:05 +0530

    [Raghav] Add: generic problem to find max of 3 floating point numbers

--- GenericProblems/Generic.cs
+++ GenericProblems/Generic.cs
@@ -10,13 +10,20 @@ namespace GenericProblems
     {
         public static int CheckMaximum(int number1, int number2, int number3)
         {
-            
-            int maximum = number1.CompareTo(number2) == 1 ? number1 : number2;
 
-          
+            int maximum = number1.CompareTo(number2) == 1 ? number1 : number2;
             maximum = maximum.CompareTo(number3) == 1 ? maximum : number3;
             Console.WriteLine("The maximum number is {0}", maximum);
+            Console.WriteLine("\n");
             return maximum;
+        }
+
+        public static float CheckMaximumFloat(float number4, float number5, float number6)
+        {
+            float maximumFloat = number4.CompareTo(number5) == 1 ? number4 : number5;
+            maximumFloat = maximumFloat.CompareTo(number6) == 1 ? maximumFloat : number6;
+            Console.WriteLine("The maximum float number is {0}", maximumFloat);
+            return maximumFloat;
         }
     }
 }
--- GenericProblems/Program.cs
+++ GenericProblems/Program.cs
@@ -5,8 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Generic Method");
-            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("------------------------Interger Maximum check-----------------------");
             int maximum = Generic.CheckMaximum(15, 30, 45);
+            Console.WriteLine("-----------------------Float Maximum check-----------------------");
+            float checkMax = Generic.CheckMaximumFloat(2.3f,4.5f,9.8f);
         }
     }
 }
