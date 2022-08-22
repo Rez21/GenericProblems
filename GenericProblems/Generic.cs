4da41e62db6f0c578ad0e9f584e758df50eade0d
Prasad <banprasad21@gmail.com>
2022-08-23 02:24:00 +0530

    [Raghav] Add: Generic concept program to find maximum of 3 integers

---
+++ GenericProblems/Generic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProblems
+{
+    internal class Generic
+    {
+        public static int CheckMaximum(int number1, int number2, int number3)
+        {
+            
+            int maximum = number1.CompareTo(number2) == 1 ? number1 : number2;
+
+          
+            maximum = maximum.CompareTo(number3) == 1 ? maximum : number3;
+            Console.WriteLine("The maximum number is {0}", maximum);
+            return maximum;
+        }
+    }
+}
---
+++ GenericProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Generic Method");
+            Console.WriteLine("-----------------------------------------------");
+            int maximum = Generic.CheckMaximum(15, 30, 45);
+        }
+    }
+}
